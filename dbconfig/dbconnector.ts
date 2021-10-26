6641e77f024dd05191a1fe929a6d0ad612ae1f0b
DuongSonn <duongson212998@gmail.com>
2021-10-26 14:14:19 +0700

    Fix env

--- dbconfig/dbconnector.ts
+++ dbconfig/dbconnector.ts
@@ -1,7 +1,8 @@
 import { Pool } from 'pg';
+require('dotenv').config()
 
 export default new Pool ({
     max: 20,
-    connectionString: `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`,
     idleTimeoutMillis: 30000
 });
